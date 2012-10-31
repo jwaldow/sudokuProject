b7c0f55e5384b67b4ae4199ec9b56ac2fa887a5b
Joshua Lamson <jlamson@mymail.mines.edu>
2012-10-31 02:01:57 -0600

    merged from hard_coded_canvases:

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Tic&dash;Tac&dash;Toe</title>
+<title>Sudoku</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="style.css" type="text/css">
 <script type="text/javascript" src="sudoku.js"></script>
 </head>
 <body>
 <div id="content">
-	<h1>HTML5 Tic&dash;Tac&dash;Toe</h1>
-	<div id="tictactoe_background">
-	<div id="tictactoe_board"></div>
+	<h1>HTML5 Sudoku</h1>
+	<div id="background">
+		<div id="board">
+		<?php for($bigi=1; $bigi<10; $bigi+=3) { ?>
+			<?php for($bigj=1; $bigj<10; $bigj+=3) { ?>
+				<div id="<?php echo "chunk_r".(floor($bigi/3)+1)."c".(floor($bigj/3)+1);?>"  width="160" height="160">
+				<?php for($i=$bigi; $i<$bigi+3; $i++) { ?>
+					<?php for($j=$bigj; $j<$bigj+3; $j++) { ?>
+							<canvas class="square" id="<?php echo "row".$i."_col".$j; ?>"></canvas>
+					<?php } ?> <br />
+				<?php } ?>	
+				</div>
+			<?php } ?>
+		<?php } ?>
+		</div>
 	</div>
 	<div id="controls">
 		<button id="reset">Reset</button>

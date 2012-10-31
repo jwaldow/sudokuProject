e17c6bbf4f614cb95bb6853fe6ad56654ccef437
Joshua Lamson <jlamson@mymail.mines.edu>
2012-10-31 00:42:23 -0600

    removed old. implemented onclick for a square

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Tic&dash;Tac&dash;Toe</title>
+<title>Sudoku</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="style.css" type="text/css">
-<!--<script type="text/javascript" src="sudoku.js"></script>-->
+<script type="text/javascript" src="sudoku.js"></script>
 </head>
 <body>
 <div id="content">
 	<h1>HTML5 Sudoku</h1>
-	<div id="tictactoe_background">
-		<div id="tictactoe_board">
-			<?php 
+	<div id="background">
+		<div id="board">
+		<?php 
 			for($i=1; $i<10; $i++) {
 				for($j=1; $j<10; $j++) { ?>
 				<canvas class="square" id="<?php echo "row".$i."_col".$j; ?>"></canvas>
-				<?php 
-				}
-			} ?>	
+		<?php	}
+			} 
+		?>	
 		</div>
 	</div>
 	<div id="controls">
--- sudoku.js
+++ sudoku.js
@@ -1,114 +1,48 @@
+var selected = null;
 
 window.onload = function() {
-
-	//Create 9 canvases for the 9 squares in the game
-	for(i=0;i<81;i++){
-		var c = document.createElement("canvas");
-		//sets the row of the square (starts from 1)
-		c.setAttribute('row', (Math.floor(i/9)+1));
-		//sets the col of the grid (starts from 1)
-		c.setAttribute('col', ((i%9)+1));
-		c.setAttribute('id','outerCanvas'+(i+1));			
-		c.setAttribute('width',50);					
-		c.setAttribute('height',50);
-		c.className = i;
-										
-		
-		c.onmouseover = pickSquare;						
-		c.onmouseout = mouseOut;
-		c.onclick = makeChoice;
-		
-		document.getElementById("tictactoe_board").appendChild(c);		
-		//Make some HTML5 modifications to each canvas
-		var ctx=c.getContext('2d');	
-		ctx.fillStyle='#FFFFFF';							
-		if ( c.getAttribute('col') == '4' || c.getAttribute('col') == '7'){
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(5, 5, 50, 50);
-            } else {
-             	ctx.fillRect(5, 0, 50, 50);
-           	
-            }
-		} else if (c.getAttribute('col') == '1'){								
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(0, 5, 50, 50);
-            } else {
-            	ctx.fillRect(0, 0, 50, 50);
-            }
-		} else {
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(2, 5, 50, 50);
-            } else {
-             	ctx.fillRect(2, 0, 50, 50);
-           	
-            }
+	for(i = 1; i < 10; i++) {
+		for(j = 1; j < 10; j++) {
+			var c = document.getElementById("row"+i+"_col"+j);
+											
+			c.onmouseover = pickSquare;						
+			c.onmouseout = mouseOut;
+			c.onclick = makeChoice;
+			
+			var ctx=c.getContext('2d');	
+			ctx.fillStyle='#FFFFFF';
+			ctx.fillRect(0, 0, 1000, 1000);									
 		}
 	}
-	
-
 }
-
-
 
 function mouseOut(){
+	var c = document.getElementById(this.id);
+	var ctx=c.getContext('2d');	
+	ctx.fillStyle='#FFFFFF';			
+	ctx.fillRect(0, 0, 1000, 1000);	
 
-		var c = document.getElementById(this.id);
-		var ctx=c.getContext('2d');	
-		ctx.fillStyle='#FFFFFF';							
-		if ( c.getAttribute('col') == '4' || c.getAttribute('col') == '7'){
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(5, 5, 50, 50);
-            } else {
-             	ctx.fillRect(5, 0, 50, 50);
-           	
-            }
-		} else if (c.getAttribute('col') == '1'){								
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(0, 5, 50, 50);
-            } else {
-            	ctx.fillRect(0, 0, 50, 50);
-            }
-		} else {
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(2, 5, 50, 50);
-            } else {
-             	ctx.fillRect(2, 0, 50, 50);
-           	
-            }
-		}
+	var ctx = selected.getContext('2d');	
+	ctx.fillStyle='#FF0000';
+	ctx.fillRect(0, 0, 1000, 1000);				
 }
 
-
-
 function makeChoice(){
+	if(selected != null) {
+		var ctx = selected.getContext('2d');	
+		ctx.fillStyle='#FFFFFF';			
+		ctx.fillRect(0, 0, 1000, 1000);
+	}
 
+	selected = document.getElementById(this.id);
+	var ctx = selected.getContext('2d');	
+	ctx.fillStyle='#FF0000';
+	ctx.fillRect(0, 0, 1000, 1000);	
 }
 
 function pickSquare(){
-
-		var c = document.getElementById(this.id);
-		var ctx=c.getContext('2d');	
-		ctx.fillStyle='#0278CC';							
-		if ( c.getAttribute('col') == '4' || c.getAttribute('col') == '7'){
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(5, 5, 50, 50);
-            } else {
-             	ctx.fillRect(5, 0, 50, 50);
-           	
-            }
-		} else if (c.getAttribute('col') == '1'){								
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(0, 5, 50, 50);
-            } else {
-            	ctx.fillRect(0, 0, 50, 50);
-            }
-		} else {
-			if (c.getAttribute('row') == '4' || c.getAttribute('row') == '7'){
-            	ctx.fillRect(2, 5, 50, 50);
-            } else {
-             	ctx.fillRect(2, 0, 50, 50);
-           	
-            }
-		}
+	var c = document.getElementById(this.id);
+	var ctx=c.getContext('2d');	
+	ctx.fillStyle='#0278CC';
+	ctx.fillRect(0, 0, 1000, 1000);	
 }
-
